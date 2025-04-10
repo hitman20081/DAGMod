1e9baab223b922e5a5107b765a252e47aabd8c27
hitman20081 <97413293+hitman20081@users.noreply.github.com>
2025-04-10 16:48:56 -0500

    Initial commit

---
+++ src/main/java/com/github/hitman20081/dagmod/DagMod.java
@@ -0,0 +1,20 @@
+package com.github.hitman20081.dagmod;
+
+import com.github.hitman20081.dagmod.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DagMod implements ModInitializer {
+	public static final String MOD_ID = "dagmod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItems.initialize();
+		ModItems.registerModItems();
+
+	}
+}
---
+++ src/main/java/com/github/hitman20081/dagmod/item/ModItems.java
@@ -0,0 +1,50 @@
+package com.github.hitman20081.dagmod.item;
+
+import com.github.hitman20081.dagmod.DagMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
+public class ModItems {
+
+    public static final Item SUSPICIOUS_SUBSTANCE = register("suspicious_substance", Item::new, new Item.Settings());
+    public static final Item AMETHYST_POWDER = register("amethyst_powder", Item::new, new Item.Settings());
+    public static final Item QUARTZ_POWDER = register("quartz_powder", Item::new, new Item.Settings());
+
+    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+        // Create the item key.
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(DagMod.MOD_ID, name));
+
+        // Create the item instance.
+        Item item = itemFactory.apply(settings.registryKey(itemKey));
+
+        // Register the item.
+        Registry.register(Registries.ITEM, itemKey, item);
+
+        return item;
+    }
+
+    public static void initialize() {
+        // Get the event for modifying entries in the ingredients group.
+// And register an event handler that adds our suspicious item to the ingredients group.
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register((itemGroup) ->
+                        itemGroup.add(ModItems.SUSPICIOUS_SUBSTANCE));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register((itemGroup) ->
+                        itemGroup.add(ModItems.AMETHYST_POWDER));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register((itemGroup) ->
+                        itemGroup.add(ModItems.QUARTZ_POWDER));
+    }
+
+    public static void registerModItems() {
+    }
+}
